43ebd31bddf781d2929890f0a59c2860f76be4b6
sheigel <eigel.si@gmail.com>
2016-01-08 10:55:30 +0200

    removed react stuff and made simple ko viewmodel work

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import {render} from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './containers/App';
-import configureStore from './store/configureStore';
 import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 
-const store = configureStore();
+import ko from 'knockout';
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('app')
-);
+let viewModel =function(){
+
+  let someText=ko.observable(10),
+   click=()=>{someText(someText()+1)}
+
+  return {
+    someText,
+    click
+  }
+}
+
+ko.applyBindings(viewModel);
+
+
